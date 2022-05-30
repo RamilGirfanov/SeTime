690dd4def3a98b2254c49a98efe3468dc50d24f8
Ramil Girfanov <ramil.girfanov@gmail.com>
2022-05-31 00:01:05 +0900

    Добавил Outlet и Action

--- SeTime/ViewController.swift
+++ SeTime/ViewController.swift
@@ -11,9 +11,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+//    MARK: - Outlets
+    
+    @IBOutlet weak var graphicView: UIView!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var workButton: UIButton!
+    
+    @IBOutlet weak var breakButton: UIButton!
+    
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var taskField: UITextField!
+    
+    @IBOutlet weak var startTaskButton: UIButton!
+    
+    @IBOutlet weak var tasksTableView: UITableView!
+    
+//    MARK: - Actions
+    
+    @IBAction func workButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func breakButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func stopButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func startTaskButtonAction(_ sender: Any) {
+    }
+    
+    
 }
 
