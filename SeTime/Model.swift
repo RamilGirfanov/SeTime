9456e0abc69ac3f130036c7d92ebe292a823bb42
Ramil Girfanov <ramil.girfanov@gmail.com>
2022-06-04 16:44:34 +0900

    Накидал примерный образ Дня и Архива

--- SeTime/Model.swift
+++ SeTime/Model.swift
@@ -8,3 +8,12 @@
 import UIKit
 
 //В этом файле будет бизнес-логика
+
+struct Day {
+    let data: Int?
+    var workTime: Int?
+    var breakTime: Int?
+    
+//    Массив с типом данных кортеж, в кортеже продолжительность и задача
+    var tasks: [(task: String, lasting: Int)]?
+}
