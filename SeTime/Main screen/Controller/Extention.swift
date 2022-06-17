34b2711e5f3b8fcecb99db0198ceadfc0867af17
Ramil Girfanov <ramil.girfanov@gmail.com>
2022-06-17 16:59:16 +0900

    Переработал методы запуска таймеров - вынес @objc функцию в замыкание Переделал надписи в графическом интерфейсе

--- SeTime/Main screen/Controller/Extention.swift
+++ SeTime/Main screen/Controller/Extention.swift
@@ -31,54 +31,49 @@ extension ViewController: TimeTasksManagement {
     
 //    Запускает таймер работы
     func startWorkTimer() {
-        dataManager.workTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeOfWork), userInfo: nil, repeats: true)
-    }
-    
-    @objc func updateTimeOfWork() {
-        day.workTime += 1
-        
-        if day.workTime < 60 {
-            workTimeLabel.text = "\(day.workTime)с"
-        } else if day.workTime < 3600 {
-            workTimeLabel.text = "\(day.workTime / 60)м \(day.workTime % 60)с"
-        } else {
-            workTimeLabel.text = "\(day.workTime / 3600)ч \((day.workTime % 3600) / 60)м \((day.workTime % 3600) % 60)с"
-        }
-        
-        if day.totalTime < 60 {
-            totalTimeLabel.text = "\(day.totalTime)с"
-        } else if day.totalTime < 3600 {
-            totalTimeLabel.text = "\(day.totalTime / 60)м \(day.totalTime % 60)с"
-        } else {
-            totalTimeLabel.text = "\(day.totalTime / 3600)ч \((day.totalTime % 3600) / 60)м \((day.totalTime % 3600) % 60)с"
-        }
-        
+        dataManager.workTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [self] _ in
+            day.workTime += 1
+            
+            if day.workTime < 60 {
+                workTimeLabel.text = "\(day.workTime)с"
+            } else if day.workTime < 3600 {
+                workTimeLabel.text = "\(day.workTime / 60)м \(day.workTime % 60)с"
+            } else {
+                workTimeLabel.text = "\(day.workTime / 3600)ч \((day.workTime % 3600) / 60)м \((day.workTime % 3600) % 60)с"
+            }
+            
+            if day.totalTime < 60 {
+                totalTimeLabel.text = "\(day.totalTime)с"
+            } else if day.totalTime < 3600 {
+                totalTimeLabel.text = "\(day.totalTime / 60)м \(day.totalTime % 60)с"
+            } else {
+                totalTimeLabel.text = "\(day.totalTime / 3600)ч \((day.totalTime % 3600) / 60)м \((day.totalTime % 3600) % 60)с"
+            }
+            
+        })
     }
     
 //    Запускает таймер перерывов
     func startBreakTimer() {
-        dataManager.breakTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimeOfBreak), userInfo: nil, repeats: true)
-    }
-    
-    @objc func updateTimeOfBreak() {
-        day.breakTime += 1
-        
-        if day.breakTime < 60 {
-            breakTimeLabel.text = "\(day.breakTime)с"
-        } else if day.breakTime < 3600 {
-            breakTimeLabel.text = "\(day.breakTime / 60)м \(day.breakTime % 60)с"
-        } else {
-            breakTimeLabel.text = "\(day.breakTime / 3600)ч \((day.breakTime % 3600) / 60)м \((day.breakTime % 3600) % 60)с"
-        }
-        
-        if day.totalTime < 60 {
-            totalTimeLabel.text = "\(day.totalTime)с"
-        } else if day.totalTime < 3600 {
-            totalTimeLabel.text = "\(day.totalTime / 60)м \(day.totalTime % 60)с"
-        } else {
-            totalTimeLabel.text = "\(day.totalTime / 3600)ч \((day.totalTime % 3600) / 60)м \((day.totalTime % 3600) % 60)с"
-        }
-        
+        dataManager.breakTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [self] _ in
+            day.breakTime += 1
+            
+            if day.breakTime < 60 {
+                breakTimeLabel.text = "\(day.breakTime)с"
+            } else if day.breakTime < 3600 {
+                breakTimeLabel.text = "\(day.breakTime / 60)м \(day.breakTime % 60)с"
+            } else {
+                breakTimeLabel.text = "\(day.breakTime / 3600)ч \((day.breakTime % 3600) / 60)м \((day.breakTime % 3600) % 60)с"
+            }
+            
+            if day.totalTime < 60 {
+                totalTimeLabel.text = "\(day.totalTime)с"
+            } else if day.totalTime < 3600 {
+                totalTimeLabel.text = "\(day.totalTime / 60)м \(day.totalTime % 60)с"
+            } else {
+                totalTimeLabel.text = "\(day.totalTime / 3600)ч \((day.totalTime % 3600) / 60)м \((day.totalTime % 3600) % 60)с"
+            }
+        })
     }
     
 //    Пауза для таймера работы
